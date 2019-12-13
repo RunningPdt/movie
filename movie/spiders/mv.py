62789d088a36b786e10b4e740af1c276de735ebe
RunningPdt <angelish2011@163.com>
2019-12-13 15:34:58 +0800

    test

--- movie/spiders/mv.py
+++ movie/spiders/mv.py
@@ -12,20 +12,24 @@ class MvSpider(scrapy.Spider):
         item = MovieItem()
         for sel in response.xpath('//div[@class="info"]'):
             title = sel.xpath('div[@class="hd"]/a/span/text()').extract()
+            print(title)
             full_title = ''
             for each in title:
                 full_title += each
             movie_info = sel.xpath('div[@class="bd"]/p/text()').extract()
+            print(movie_info)
             star = sel.xpath('div[@class="bd"]/div[@class="star"]/span[@class="rating_num"]/text()').extract()[0]
+            print(star)
             quote = sel.xpath('div[@class="bd"]/p/span/text()').extract()
+            print(quote)
             if quote:
                 quote = quote[0]
             else:
                 quote = ''
-            item['title'] = full_title
-            item['movie_info'] = ';'.join(movie_info).replace(' ', '').replace('\n', '')
-            item['star'] = star[0]
-            item['quote'] = quote
+            item['title'] = full_title.replace(u'\xa0', u'')
+            item['movie_info'] = ';'.join(movie_info).replace(' ', '').replace('\n', '').replace(u'\xa0', u'')
+            item['star'] = star[0].replace(u'\xa0', u'')
+            item['quote'] = quote.replace(u'\xa0', u'')
             yield item
         next_page = response.xpath('//span[@class="next"]/link/@href').extract()
         if next_page:
